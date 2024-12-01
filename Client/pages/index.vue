e25de8483e3e4ce9a4781c145f82fbad192a6e7a
Edanriell <Edanriell@gmail.com>
2024-12-01 14:35:58 +0200

    feature(main-layout): created main layout

---
+++ Client/pages/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+	import { HomePage } from "~/nuxt-pages/home/ui";
+
+	useSeoMeta({
+		title: "Body Mass Index Calculator",
+		ogTitle: "Body Mass Index Calculator",
+		description:
+			"Easily calculate your Body Mass Index (BMI) to understand your health status. Input your height and weight to get quick and accurate results.",
+		ogDescription:
+			"Quickly calculate your BMI to assess your health status. Enter your height and weight for instant insights into your body mass index."
+	});
+
+	definePageMeta({
+		layout: "main"
+	});
+</script>
+
+<template>
+	<HomePage />
+</template>
